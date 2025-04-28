9a8f42e6fac46c92ce379587f831524cd29b77d8
Yangsb <ysb5397@gmail.com>
2025-04-28 17:57:48 +0900

    기타 테스트

---
+++ src/_my_test/FunctionTest.java
@@ -0,0 +1,39 @@
+package _my_test;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FunctionTest extends JFrame {
+
+    private drawPaint drawPaint;
+
+    public FunctionTest() {
+        initData();
+        setInitData();
+    }
+
+    private void initData() {
+        setTitle("함수 테스트");
+        setSize(800,800);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        drawPaint = new drawPaint();
+    }
+
+    private void setInitData() {
+        super.add(drawPaint);
+    }
+
+    static class drawPaint extends JPanel {
+        @Override
+        public void paint(Graphics g) {
+            for (int i = 0; i < getWidth(); i++)
+                g.drawLine((i * i), (10 * i), (i * i), (10 * i));
+        }
+    }
+
+    public static void main(String[] args) {
+        FunctionTest functionTest = new FunctionTest();
+    }
+}
